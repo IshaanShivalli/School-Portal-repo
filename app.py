cd4bfa8591c0419a731d27287a7ae9ff448f1754
IshaanShivalli <smilewithishaan25@gmail.com>
2026-03-08 19:44:09 +0530

    Testing for favicon

--- app.py
+++ app.py
@@ -355,6 +355,15 @@ def feedback():
     """)
     return render_template("feedback.html", success=success, error=error, feedbacks=feedbacks)
 
+from flask import send_from_directory
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, 'static'),
+        'favicon.ico',
+        mimetype='image/x-icon'
+    )
 
 @app.route("/school_news")
 @login_required
